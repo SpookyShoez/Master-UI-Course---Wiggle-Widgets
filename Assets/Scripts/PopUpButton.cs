f1a78d664f2bed8ae842e3fbef4d9d7d267ba333
Morialle Barr <Moriallekbarr@gmail.com>
2025-11-16 11:02:32 +0200

    PopUp Fixed

--- Assets/Scripts/PopUpButton.cs
+++ Assets/Scripts/PopUpButton.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PopUpButton : MonoBehaviour
 {
+    public Transform content;
+    public CanvasGroup darkCanvasGroup;
+
     private void OnEnable()
     {
-        transform.localScale = Vector3.zero;//(0,0,0) (1,1,1)
+        transform.localScale = Vector3.zero;//(0,0,0) (1,)
         transform.LeanScale(Vector3.one, 0.5f).setEaseOutBack();
+
+        darkCanvasGroup.alpha = 0;
+        darkCanvasGroup.LeanAlpha(1f, 0.5f);
     }
 
     public void OnClose()
     {
         transform.LeanScale(Vector3.zero, 0.5f).setEaseInOutSine().setOnComplete(() => {
-            gameObject.SetActive(false);
+        gameObject.SetActive(false);
         });
+        darkCanvasGroup.LeanAlpha(0f, 0.5f);
     }       
 }
+
+
 
